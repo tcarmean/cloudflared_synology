3e246e28986fec390ec4eab5797f985555c825b1
Tim Carmean <tecarmea@mtu.edu>
2018-05-13 16:30:55 -0400

    redirecting stderr to stdout

--- S99CloudFlared.sh
+++ S99CloudFlared.sh
@@ -6,12 +6,12 @@ PIDFILE="/var/run/cloudflared.pid"
 
 case "$1" in
 	start)
-		$CLOUDFLARED --config ./CloudFlared.yml > /dev/null &
+		$CLOUDFLARED --config ./CloudFlared.yml > /dev/null 2&>1 &
 		printf "[%4s]\n" "done"
 		;;
 	stop)
 		printf "%-30s" "Stopping CloudFlared"
-		kill `cat $PIDFILE` > /dev/null
+		kill `cat $PIDFILE` > /dev/null 2&>1
 		rm $PIDFILE
 		printf "[%4s]\n" "done"
 		;;
