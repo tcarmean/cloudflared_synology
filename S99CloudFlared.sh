c427f231c28debec37f3f2c54453d754278091fb
Tim Carmean <tecarmea@mtu.edu>
2018-05-13 16:29:41 -0400

    redirecting stdout to /dev/null

--- S99CloudFlared.sh
+++ S99CloudFlared.sh
@@ -6,12 +6,12 @@ PIDFILE="/var/run/cloudflared.pid"
 
 case "$1" in
 	start)
-		$CLOUDFLARED --config ./CloudFlared.yml &
+		$CLOUDFLARED --config ./CloudFlared.yml > /dev/null &
 		printf "[%4s]\n" "done"
 		;;
 	stop)
 		printf "%-30s" "Stopping CloudFlared"
-		kill `cat $PIDFILE`
+		kill `cat $PIDFILE` > /dev/null
 		rm $PIDFILE
 		printf "[%4s]\n" "done"
 		;;
