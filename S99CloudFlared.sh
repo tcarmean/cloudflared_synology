e697220937fcf454b45cc56a0fcab1ce855e173a
Tim Carmean <tecarmea@mtu.edu>
2018-05-13 14:56:26 -0400

    fuck yaml

--- S99CloudFlared.sh
+++ S99CloudFlared.sh
@@ -5,11 +5,12 @@ PIDFILE="/var/run/cloudflared.pid"
 
 case "$1" in
 	start)
-		/volume1/@appstore/cloudflared/cloudflared --config /volume1/@appstore/cloudflared/CloudFlared.yml &
+		/volume1/@appstore/cloudflared/cloudflared proxy-dns --address 0.0.0.0 --pidfile $PIDFILE &
 		printf "[%4s]\n" "done"
 		;;
 	stop)
 		printf "%-30s" "Stopping CloudFlared"
+		kill < $PIDFILE
 		rm $PIDFILE
 		printf "[%4s]\n" "done"
 		;;
